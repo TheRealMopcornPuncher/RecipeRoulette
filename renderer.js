d5ecffa830e3947c18e4ab5651b60f729d1cee68
Amaya <m0pcornpuncher@gmail.com>
2024-12-24 22:05:22 -0600

    Create electron template

---
+++ renderer.js
@@ -0,0 +1,10 @@
+window.addEventListener('DOMContentLoaded', () => {
+  const replaceText = (selector, text) => {
+    const element = document.getElementById(selector);
+    if (element) element.innerText = text;
+  }
+
+  replaceText('node-version', process.versions.node);
+  replaceText('chrome-version', process.versions.chrome);
+  replaceText('electron-version', process.versions.electron);
+});
